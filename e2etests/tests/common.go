c53cb7f72ae67b2943664b5743fbd73095310208
Andrea Panattoni <apanatto@redhat.com>
2026-09-10 12:41:24 +0200

    fix(e2e): Move dump logic to its own package
    Logic in dump can be useful to other e2etest suites,
    but they can't import it without registering the Ginkgo
    tests in `e2etests/tests`, which happens when importing
    the package.
    
    Move the logic to a shared `e2etests/triage` package.
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- e2etests/tests/common.go
+++ e2etests/tests/common.go
@@ -5,7 +5,10 @@ package tests
 import (
 	"github.com/onsi/ginkgo/v2"
 	"github.com/openperouter/openperouter/e2etests/pkg/config"
+	"github.com/openperouter/openperouter/e2etests/triage"
 	"github.com/openshift-kni/k8sreporter"
+	corev1 "k8s.io/api/core/v1"
+	clientset "k8s.io/client-go/kubernetes"
 )
 
 var (
@@ -18,3 +21,19 @@ var (
 )
 
 var GroutSupport = ginkgo.Label("grout-support")
+
+func dumpIfFails(cs clientset.Interface, additionalNamespaces ...string) {
+	triage.DumpIfFails(cs, triage.Config{
+		ReportPath:           ReportPath,
+		HostMode:             HostMode,
+		GroutMode:            GroutMode,
+		K8sReporter:          K8sReporter,
+		IncludeFRRK8sPods:    true,
+		IncludeFRRContainers: true,
+		IncludePodman:        HostMode,
+	}, additionalNamespaces...)
+}
+
+func DumpPods(name string, pods []*corev1.Pod) {
+	triage.DumpPods(name, pods)
+}
